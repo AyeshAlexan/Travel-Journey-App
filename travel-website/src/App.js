aa23e81e229b64422a642af2ed654692d13f1371
Ayesh Alexan Anthonythasan <ayeshanthoythasan@gmail.com>
2025-08-08 16:29:18 +0530

    Finish the TopDestination.jsx

--- travel-website/src/App.js
+++ travel-website/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import HeroSection from './components/HeroSection';
 import TrendingSection from './components/TrendingSection';
+import TopDestinations from './components/TopDestinations';
 
 const App = () => {
   return (
     <>
       <HeroSection />
       <TrendingSection />
-      
+      <TopDestinations />
     </>
   );
 };
--- travel-website/src/components/TopDestinations.jsx
+++ travel-website/src/components/TopDestinations.jsx
@@ -4,8 +4,26 @@ import kandy from "../assets/kandy1.jpeg";
 import ella from "../assets/ella-tr.jpg";
 import jaffna from "../assets/jaffna.jpg";
 
-const topDestinations = [
+const TopDestinations = () => {
+const destinations = [
     {img :kandy,label: "Kandy"},
-    {ima : ella,label: "Ella"},
-    {img : jaffna,label: "Jaffna"}
-]
+    {img : ella,label: "Ella"},
+    {img : jaffna,label: "Jaffna"},
+];
+
+return(
+    <section className="top-destinations">
+        <h2>Top Destinations</h2>
+        <div className="destination-grid">
+            {destinations.map((place, index)=>(
+                <div className="destination-card" key={index}>
+                    <img src={place.img} alt={place.label}/>
+                    <div className="destination-label">{place.label}</div>
+                </div>
+            ))}
+        </div>
+    </section>
+);
+};
+
+export default TopDestinations;
