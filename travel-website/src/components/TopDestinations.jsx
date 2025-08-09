0be1b22ece9331afc29b44fef520f09a73ac021f
Ayesh Alexan Anthonythasan <ayeshanthoythasan@gmail.com>
2025-08-09 11:16:40 +0530

    Update Top Destinations section with additional images and adjust heading font size

--- travel-website/src/components/TopDestinations.jsx
+++ travel-website/src/components/TopDestinations.jsx
@@ -3,12 +3,18 @@ import styles from "../styles/TopDestinations.css";
 import kandy from "../assets/kandy1.jpeg";
 import ella from "../assets/ella-tr.jpg";
 import jaffna from "../assets/jaffna.jpg";
+import waterfall from "../assets/waterfalls.jpg";
+import colombo from "../assets/colombo.jpg";
+import gallfort from "../assets/galle-fort.jpg";
 
 const TopDestinations = () => {
 const destinations = [
     {img :kandy,label: "Kandy"},
     {img : ella,label: "Ella"},
     {img : jaffna,label: "Jaffna"},
+    {img : waterfall,label: "Waterfall"},
+    {img : colombo,label: "Colombo"},   
+    {img : gallfort,label: "Galle Fort"}
 ];
 
 return(
