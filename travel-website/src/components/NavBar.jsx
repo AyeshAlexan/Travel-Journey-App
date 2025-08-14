4c356e52b50e83998dd27734ac4f7667a7da6050
Ayesh Alexan Anthonythasan <ayeshanthoythasan@gmail.com>
2025-08-14 09:37:27 +0530

    Change the Home Page into Pages folder and make the nav bar as react-router-dom

--- travel-website/src/components/NavBar.jsx
+++ travel-website/src/components/NavBar.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import styles from '../styles/NavBar.css';
+import { Link } from 'react-router-dom';
+import '../styles/NavBar.css';
 
 function NavBar() {
   return (
-       <nav className="hero-nav">
-        <div className="logo">Travel Lanka</div>
-        <ul className="nav-links">
-        <a href="../components/HeroSection.jsx">Home</a>
-        <a href="../components/About.jsx">About</a>
-        <a href="../components/Explore.jsx">Explore</a>
-        <a href="../components/Contact.jsx">Contact</a>
-        </ul>
-        <div className="social-icons">
-          <i className="fab fa-facebook-f"></i>
-          <i className="fab fa-instagram"></i>
-          <i className="fab fa-linkedin-in"></i>
-        </div>
-      </nav>
-    
+    <nav className="hero-nav">
+      <div className="logo">Travel Lanka</div>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/explore">Explore</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+      <div className="social-icons">
+        <i className="fab fa-facebook-f"></i>
+        <i className="fab fa-instagram"></i>
+        <i className="fab fa-linkedin-in"></i>
+      </div>
+    </nav>
   );
 }
 
