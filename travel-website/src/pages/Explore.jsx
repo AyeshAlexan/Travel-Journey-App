78c08577e2ebfb2dd51e86b1a0f8baf3c1c47fd7
Ayesh Alexan Anthonythasan <ayeshanthoythasan@gmail.com>
2025-08-16 18:31:44 +0530

    Implement code changes to enhance functionality and improve performance

--- travel-website/src/pages/Explore.jsx
+++ travel-website/src/pages/Explore.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-import styles from "../styles/Explore.css";
+import React  from "react";
 
 const Explore = () => {
-    const destinations = [
-        { name: "WaterFalls", image: "../assets/waterfalls.jpg" },
-        { name: "New York", image: "../assets/newyork.jpg" },
-        { name: "Tokyo", image: "../assets/tokyo.jpg" },
-        { name: "Sydney", image: "../assets/sydney.jpg" }
-    ];
+    const beaches = [
+        { name: "Unawatuna Beach",
+        img : 
+}}
+    ]
 }
